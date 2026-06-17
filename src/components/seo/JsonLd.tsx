ffd4cabcce251cd0da11896d01d78c50444cb386
Maddala Jashwanth <maddalajashwanth69@gmail.com>
2026-06-17 12:15:16 +0530

    fix(seo): improve JSON-LD robustness with character escaping

--- src/components/seo/JsonLd.tsx
+++ src/components/seo/JsonLd.tsx
@@ -3,10 +3,16 @@ interface JsonLdProps {
 }
 
 export default function JsonLd({ data }: JsonLdProps) {
+  // Defensive escaping: Replace characters that can break HTML parsing
+  const jsonString = JSON.stringify(data)
+    .replace(/</g, '\\u003c')
+    .replace(/>/g, '\\u003e')
+    .replace(/&/g, '\\u0026');
+
   return (
     <script
       type="application/ld+json"
-      dangerouslySetInnerHTML={{ __html: JSON.stringify(data) }}
+      dangerouslySetInnerHTML={{ __html: jsonString }}
     />
   );
 }
